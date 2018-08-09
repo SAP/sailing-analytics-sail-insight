f496a7da86651804504319dca1afce37ecb109d7
Dorian Kanitz <dorian.kanitz@d-labs.com>
2018-08-09 12:13:44 +0200

    jest config

--- src/services/__tests__/TestCheckInService.js
+++ src/services/__tests__/TestCheckInService.js
@@ -1,6 +1,5 @@
 import CheckInService from '../CheckInService'
 
-
 const testUrl = 'https://d-labs.sapsailing.com/tracking/checkin?event_id=750d5773-77f9-4e8d-a704-9231b3ed435f&leaderboard_name=Regatta+Dorian&competitor_id=cf86ae7a-bb32-481e-94bc-246d3699a11b'
 
 
