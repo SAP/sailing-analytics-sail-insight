e5531e4cea896e905f36ce508b816a819ce5e1bc
Dorian Kanitz <dorian.kanitz@d-labs.com>
2018-08-21 14:23:52 +0200

    remove test checkIns, status bar platform indepent coloring

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,4 +1,4 @@
-import { StatusBar } from 'react-native'
+import { Platform, StatusBar } from 'react-native'
 import EStyleSheet from 'react-native-extended-stylesheet'
 
 import * as colors from 'styles/colors'
@@ -6,7 +6,9 @@ import * as dimensions from 'styles/dimensions'
 
 export const initStyles = () => {
   StatusBar.setBarStyle('dark-content')
-  StatusBar.setBackgroundColor(colors.$containerBackgroundColor)
+  if (Platform.OS === 'android') {
+    StatusBar.setBackgroundColor(colors.$containerBackgroundColor)
+  }
 
   EStyleSheet.build({
     ...colors,
