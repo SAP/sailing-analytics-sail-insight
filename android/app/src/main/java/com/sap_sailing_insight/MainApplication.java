fa0ae3803334429a3828d744b3670f0bd181c05b
Stefan Pratsch <stefan.pratsch@d-labs.com>
2019-02-17 14:56:49 +0100

    SI-79 add dependency react-native-firebase

--- android/app/src/main/java/com/sap_sailing_insight/MainApplication.java
+++ android/app/src/main/java/com/sap_sailing_insight/MainApplication.java
@@ -3,6 +3,7 @@ package com.sap_sailing_insight;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.apsl.versionnumber.RNVersionNumberPackage;
 import com.dylanvann.fastimage.FastImageViewPackage;
@@ -39,6 +40,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new PickerPackage(),
             new RNVersionNumberPackage(),
             new FastImageViewPackage(),
