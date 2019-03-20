154ab70e099c9ce356080c1a44c500d0d0c278c4
Stefan Pratsch <stefan.pratsch@d-labs.com>
2019-03-20 16:32:57 +0100

    Reorganice process of handling, saving and transfer gps fixes. Location updates are saved immediately. Data is transfered using GPSFixService in short period of time.

--- src/services/GPSFixService.ts
+++ src/services/GPSFixService.ts
@@ -5,17 +5,21 @@ import Logger from 'helpers/Logger'
 import { PositionFix } from 'models'
 import { deleteGPSFixRequests, readGPSFixRequests, writeGPSFixRequest } from 'storage'
 import { BASE_URL_PROPERTY_NAME, GPS_FIX_PROPERTY_NAME } from 'storage/schemas'
+import { updateUnsentGpsFixCount } from '../actions/locationTrackingData'
+import { handleManeuverChange } from '../actions/sessions'
+import { DispatchType } from '../helpers/types'
 
 import * as BackgroundTaskService from './BackgroundTaskService'
 import * as CheckInService from './CheckInService'
-// import * as LocationService from './LocationService'
 
 
-export const UPDATE_TIME_INTERVAL_IN_MILLIS = 30000
+export const DEFAULT_UPDATE_TIME_INTERVAL_IN_MILLIS = 5000
+export const BULK_UPDATE_TIME_INTERVAL_IN_MILLIS = 30000
 
 let stopWhenSynced = false
 
-export const syncFixes = async () => {
+export const syncFixes = async (dispatch: DispatchType) => {
+  Logger.debug('syncFixes')
   const fixRequests = readGPSFixRequests({ sortedBy: BASE_URL_PROPERTY_NAME })
   const urls: { [url: string]: any[]; } = {}
 
@@ -27,11 +31,22 @@ export const syncFixes = async () => {
     urls[baseUrl].push(fixRequest[GPS_FIX_PROPERTY_NAME])
   })
 
+  Logger.debug('fixRequests', fixRequests)
   await Promise.all(keys(urls).map(async (url: string) => {
     const postData = CheckInService.gpsFixPostData(urls[url])
+    Logger.debug('postData', postData)
     try {
-      await api(url).sendGpsFixes(postData)
-      deleteGPSFixRequests(fixRequests)
+      let maneuverInfo
+      try {
+        maneuverInfo = await api(url).sendGpsFixes(postData)
+        deleteGPSFixRequests(fixRequests)
+      } catch (err) {
+        Logger.debug(err)
+      } finally {
+        dispatch(handleManeuverChange(maneuverInfo))
+        dispatch(updateUnsentGpsFixCount(unsentGpsFixCount()))
+      }
+
       if (stopWhenSynced) {
         stopGPSFixUpdates()
       }
@@ -49,16 +64,20 @@ export const unsentGpsFixCount = () => {
   return fixes && fixes.length
 }
 
-export const startPeriodicalGPSFixUpdates = () => {
-  Logger.debug('[GPS-Fix] Offline Manager started')
-  BackgroundTaskService.startBackgroundTimer(UPDATE_TIME_INTERVAL_IN_MILLIS)
-  BackgroundTaskService.addTaskListener(syncFixes)
+let syncFixesWithDispatch: () => {}
+
+export const startPeriodicalGPSFixUpdates = (bulkTransfer: boolean, dispatch: DispatchType) => {
+  Logger.debug('[GPS-Fix] Transfer Manager started')
+  stopWhenSynced = false
+  BackgroundTaskService.startBackgroundTimer(bulkTransfer ? BULK_UPDATE_TIME_INTERVAL_IN_MILLIS : DEFAULT_UPDATE_TIME_INTERVAL_IN_MILLIS)
+  syncFixesWithDispatch  = () => syncFixes(dispatch)
+  BackgroundTaskService.addTaskListener(syncFixesWithDispatch)
   // LocationService.addHeartbeatListener(onTask)
 }
 
 export const stopGPSFixUpdates = () => {
-  Logger.debug('[GPS-Fix] Offline Manager stopped')
-  BackgroundTaskService.removeTaskListener(syncFixes)
+  Logger.debug('[GPS-Fix] Transfer Manager stopped')
+  BackgroundTaskService.removeTaskListener(syncFixesWithDispatch)
   BackgroundTaskService.stopBackgroundTimer()
   // LocationService.removeHeartbeatListener(onTask)
 }
