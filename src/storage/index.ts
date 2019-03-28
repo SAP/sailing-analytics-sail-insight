eb158263a0e41175b8963b4443248fa79d08bdf6
Stefan Pratsch <stefan.pratsch@d-labs.com>
2019-03-28 17:19:24 +0100

    Restructured and simplyfied background timer handling

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -30,7 +30,7 @@ export const writeGPSFixRequest = (url: string, gpsFix: PositionFix) => {
       )
     })
   } catch (e) {
-    Logger.debug('Error on creation', e)
+    Logger.error('Error on gpsfix creation', e)
   }
 }
 
@@ -42,6 +42,6 @@ export const deleteGPSFixRequests = (fixes: any) => {
       realm.delete(fixes)
     })
   } catch (e) {
-    Logger.debug('Error on deletion', e)
+    Logger.error('Error on gpsfix deletion', e)
   }
 }
