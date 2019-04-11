7fef1affa4c1966d44cf5e3f744d83f06c7e0075
Stefan Pratsch <stefan.pratsch@d-labs.com>
2019-04-11 23:26:38 +0200

    Dual storage of gps fixes. Allow export of gps fixes using long-press on version number in settings screen.

--- src/storage/realm.ts
+++ src/storage/realm.ts
@@ -1,7 +1,7 @@
 import Realm from 'realm'
-import { GPS_REQUEST_SCHEMA, GPS_SCHEMA } from './schemas'
+import { GPS_REQUEST_DUPLICATE_SCHEMA, GPS_REQUEST_SCHEMA, GPS_SCHEMA } from './schemas'
 
 
-const realm = new Realm({ schema: [GPS_SCHEMA, GPS_REQUEST_SCHEMA] })
+const realm = new Realm({ schema: [GPS_SCHEMA, GPS_REQUEST_SCHEMA, GPS_REQUEST_DUPLICATE_SCHEMA] })
 
 export default realm
