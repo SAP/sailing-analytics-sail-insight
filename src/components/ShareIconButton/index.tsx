d9cca977f9f46a23dad662fff7b900ca58dd4f99
Martynas Stankevičius <martynoemailas@gmail.com>
2019-06-20 15:13:28 +0300

    Add text to the "share tracking url" message

--- src/components/ShareIconButton/index.tsx
+++ src/components/ShareIconButton/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Image, Share, TouchableOpacity } from 'react-native'
 
 import Images from '@assets/Images'
+import I18n from 'i18n'
 
 import { button } from 'styles/commons'
 import styles from './styles'
@@ -25,8 +26,11 @@ class ShareButton extends React.Component<{
   }
 
   private onPress = () => {
-    const { url } = this.props
-    Share.share({ message: url || '' })
+    const { url = '' } = this.props
+
+    const message = `${I18n.t('text_track_share')}${url}`
+
+    Share.share({ message })
   }
 }
 
