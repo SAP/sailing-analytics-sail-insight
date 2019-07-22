eb23d6a7ff659b2b2035eb9bc0e6122574b84338
Martynas Stankevičius <martynoemailas@gmail.com>
2019-07-22 16:39:07 +0300

    Add basic Geolocation screen and functionality

--- src/containers/CourseCreation/Geolocation/index.tsx
+++ src/containers/CourseCreation/Geolocation/index.tsx
@@ -1,33 +1,69 @@
+import { __, compose, concat, map, merge, mergeLeft, reduce, range, objOf } from 'ramda'
+
 import React from 'react'
 import { View } from 'react-native'
-import EStyleSheets from 'react-native-extended-stylesheet'
+import MapView from 'react-native-maps'
 
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import {
+  Component,
+  fold,
+  fromClass,
+  nothing,
+  contramap,
+  recomposeWithState as withState
+} from 'components/fp/component'
+import { text, touchableOpacity, view } from 'components/fp/react-native'
+import TextInput from 'components/TextInput'
 
-const styles = EStyleSheets.create({
-  container: {
-    ...EStyleSheets.absoluteFillObject,
-    height: 400,
-    width: 400,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    ...EStyleSheets.absoluteFillObject,
-  },
-})
+import styles from './styles'
 
-export default () => (
-   <View style={styles.container}>
-     <MapView
-       provider={PROVIDER_GOOGLE}
-       style={styles.map}
-       region={{
-         latitude: 37.78825,
-         longitude: -122.4324,
-         latitudeDelta: 0.015,
-         longitudeDelta: 0.0121,
-       }}
-     />
-   </View>
+const initialRegion = {
+  latitude: 37.78825,
+  longitude: -122.4324,
+  latitudeDelta: 1,
+  longitudeDelta: 1,
+}
+const withRegion = withState('region', 'setRegion', initialRegion)
+
+const mapView = (settings: any) => Component((props: any) => compose(
+    fold(props),
+    view({ style: styles.mapContainer }),
+    contramap(mergeLeft({
+      onRegionChange: (region: any) => {
+        props.setRegion(region)
+      }
+    })),
+    contramap(mergeLeft(settings)),
+  )(fromClass(MapView))
+)
+
+const coordinatesInput = ({ propName }: any) => Component((props: any) => compose(
+    fold(props),
+    contramap(mergeLeft({
+      // Should add toPrecision(7)
+      value: props.region[propName].toString(),
+      onChangeText: (value: any) => props.setRegion({
+        ...props.region,
+        [propName]: value,
+      }),
+      placeholder: propName,
+    }))
+  )(fromClass(TextInput))
+)
+
+const latitudeInput = coordinatesInput({ propName: 'latitude' })
+const longitudeInput = coordinatesInput({ propName: 'longitude' })
+
+export default Component((props: object) =>
+  compose(
+    fold(props),
+    withRegion,
+    reduce(concat, nothing()),
+  )([
+    mapView({
+      style:styles.map,
+    }),
+    latitudeInput,
+    longitudeInput
+  ])
 )
