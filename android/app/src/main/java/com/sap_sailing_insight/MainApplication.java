8db11f534bb797094bf038454262884beacd8946
Martynas Stankevičius <martynoemailas@gmail.com>
2019-07-22 11:26:49 +0300

    Add react-native-maps dependency
    - Untested on iOS
    - the change to android/build.gradle was needed because of a mismatch
      of runtime and compile versions. The fix was taken from:
      https://stackoverflow.com/a/55409979
      Another probable fix is:
      https://stackoverflow.com/a/47383261

--- android/app/src/main/java/com/sap_sailing_insight/MainApplication.java
+++ android/app/src/main/java/com/sap_sailing_insight/MainApplication.java
@@ -3,6 +3,7 @@ package com.sap_sailing_insight;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.reactnativecommunity.slider.ReactSliderPackage;
 import com.rnfs.RNFSPackage;
 import io.invertase.firebase.RNFirebasePackage;
@@ -44,6 +45,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new MapsPackage(),
             new ReactSliderPackage(),
             new RNFSPackage(),
             new RNFirebasePackage(),
