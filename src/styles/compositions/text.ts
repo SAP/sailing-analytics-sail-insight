eb5c72c4e239d4f81e4d2f804027afc6181b40b9
Adrian Aioanei <adrian.aioanei@ayonesoftware.com>
2019-08-28 16:40:35 +0300

    Fixed issue with Boat class autocomplete not working in a functional environment. Visual styles for Boat class, Race number and discards.

---
+++ src/styles/compositions/text.ts
@@ -0,0 +1,15 @@
+export const withDefaultFont = {
+    fontFamily: '$defaultFontFamily'
+}
+
+export const withDefaultBoldFont = {
+    fontFamily: '$defaultBoldFontFamily'
+}
+
+export const withTitleSize = {
+    fontSize: '$titleFontSize'
+}
+
+export const white = {
+    color: 'white'
+}
