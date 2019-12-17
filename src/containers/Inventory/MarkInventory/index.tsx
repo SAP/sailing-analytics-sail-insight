27dd9de310d1945362724911840bd145a5bf6b36
Adrian Aioanei <adrian.aioanei@ayonesoftware.com>
2019-12-17 16:20:00 +0200

    Start/finish related marks placed at the bottom of a user's inventory.

--- src/containers/Inventory/MarkInventory/index.tsx
+++ src/containers/Inventory/MarkInventory/index.tsx
@@ -1,6 +1,7 @@
 import { __, compose, always, objOf,
-  prop, map, reduce, concat, merge, defaultTo } from 'ramda'
-
+  prop, map, reduce, concat, merge, defaultTo,
+  partition, flatten, reverse, includes, sortBy,
+  when, equals, indexOf, addIndex } from 'ramda'
 import {
   Component,
   fold,
@@ -11,17 +12,29 @@ import {
 } from 'components/fp/component'
 import { text, view, scrollView, touchableOpacity, forwardingPropsFlatList } from 'components/fp/react-native'
 import { ControlPointClass } from 'models/Course'
-
 import { getMarkProperties } from 'selectors/inventory'
 import { loadMarkProperties, deleteMarkProperties } from 'actions/inventory'
-
 import { Alert } from 'react-native'
 import styles from './styles'
 import IconText from 'components/IconText'
 import Images from '@assets/Images'
 
-const mapStateToProps = (state, props) => ({
-  markProperties: getMarkProperties(state)
+const mapIndexed = addIndex(map)
+
+const startFinishMarks = ['Start/Finish Pin', 'Start/Finish Boat', 'Start Pin', 'Start Boat', 'Finish Pin', 'Finish Boat']
+
+const mapStateToProps = state => ({
+  markProperties: compose(
+    flatten,
+    reverse,
+    mapIndexed((set, index) =>
+      when(
+        always(equals(0, index)),
+        sortBy(compose(indexOf(__, startFinishMarks), prop('name'))),
+        set)),
+    partition(compose(includes(__, startFinishMarks), prop('name'))),
+    getMarkProperties)(
+    state)
 })
 
 const withLoadingMarks = lifeCycle({
