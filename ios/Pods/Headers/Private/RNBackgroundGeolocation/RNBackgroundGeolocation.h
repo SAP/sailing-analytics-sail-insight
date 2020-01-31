fcf9fcaa35c379d2475ea89de7b15cd87b6a2735
Adrian Aioanei <adrian.aioanei@ayonesoftware.com>
2020-01-31 09:05:44 +0200

    Adjusted settings fro react-native 0.60

--- ios/Pods/Headers/Private/RNBackgroundGeolocation/RNBackgroundGeolocation.h
+++
@@ -1,1 +0,0 @@
-../../../../../node_modules/react-native-background-geolocation-android/ios/RNBackgroundGeolocation/RNBackgroundGeolocation.h
--- ios/Pods/Target Support Files/Pods-sap_sailing_insightTests/Pods-sap_sailing_insightTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_sap_sailing_insightTests : NSObject
-@end
-@implementation PodsDummy_Pods_sap_sailing_insightTests
-@end
