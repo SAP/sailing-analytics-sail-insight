797ad55e35a51e88ceeba9d56a7365955d1a3b4f
Radu Rad <radu.r@scopicsoftware.com>
2020-02-18 11:33:13 +0200

    Event Creation: - remove flag reset with component hooks - added flag reset on app initialisation (other flags, including checkin flag should be added as well)

--- src/actions/appLoading.ts
+++ src/actions/appLoading.ts
@@ -3,9 +3,13 @@ import { initialNavigation } from 'navigation'
 
 import { checkCurrentAuthSession } from './auth'
 import { handleAppStartDeepLink } from './deepLinking'
+import { updateCreatingEvent } from './events'
 
 
 export const initializeApp = () => async (dispatch: DispatchType) => {
+  /// reset flags for loading
+  await dispatch(updateCreatingEvent(false))
+  /// reset flags for loading
   await dispatch(checkCurrentAuthSession())
   initialNavigation()
   await dispatch(handleAppStartDeepLink())
