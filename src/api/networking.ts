98451c203b22408d56117b39241fd1b093d046a6
Sergij Rylskyj <sergij.rylskyj@gmail.com>
2020-03-21 01:20:03 +0200

    1. Removed the SINetwork locally; 2. Make use the SailInsightMTCP Network from the gitea repo; 3. Code cleanup;

--- src/api/networking.ts
+++ src/api/networking.ts
@@ -8,7 +8,7 @@ import { DEV_MODE, isPlatformAndroid } from 'environment'
 import Logger from 'helpers/Logger'
 import crashlytics from '@react-native-firebase/crashlytics'
 
-import SINetwork from 'react-native-network/index'
+import SailInsightMtcpNetwork from 'sail-insight-mtcp-network/index'
 
 const responseHasFatalError = compose(
   includes(__, [400, 404, 405]),
@@ -102,7 +102,7 @@ export const request = async (
     if (isPlatformAndroid) {
       response = await timeoutPromise(fetch(url, fetchOptions), timeout, 'Server request timeout')
     } else {
-      response = await timeoutPromise(SINetwork.fetch(url, fetchOptions), timeout, 'Server request timeout')
+      response = await timeoutPromise(SailInsightMtcpNetwork.fetch(url, fetchOptions), timeout, 'Server request timeout')
     }
   } catch (err) {
     throw err
