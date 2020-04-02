a442c965d1da6edc4c4404e10c90ad97c9fd867e
Radu Rad <radu.r@scopicsoftware.com>
2020-04-02 18:01:17 +0300

    Communication Settings: - send settings to native modules on app start and on toggle on (prepare for complete removal of hardcoded values in native modules)

--- src/actions/settings.ts
+++ src/actions/settings.ts
@@ -3,12 +3,13 @@ import { DispatchType } from 'helpers/types'
 import analytics from '@react-native-firebase/analytics'
 
 export const UPDATE_COMMUNICATION_SETTINGS = "UPDATE_COMMUNICATION_SETTINGS"
+export const UPDATE_MTCP_COMMUNICATION_SETTING = "SETTINGS_MTCP_COMMUNICATION"
 
 export const updateGpsBulkSetting    = createAction('SETTINGS_UPDATE_BULK_GPS')
 export const updateServerUrlSetting = createAction('SETTINGS_UPDATE_SERVER_URL')
 export const updateAnalyticsSettings = createAction('SETTINGS_UPDATE_ANALYTICS')
 export const updateVerboseLoggingSetting = createAction('SETTINGS_UPDATE_VERBOSE_LOGGING')
-export const updateMtcpAndCommunicationSetting = createAction('SETTINGS_MTCP_COMMUNICATION')
+export const updateMtcpAndCommunicationSetting = createAction(UPDATE_MTCP_COMMUNICATION_SETTING)
 export const updateLeaderboardEnabledSetting = createAction('SETTINGS_UPDATE_LEADERBOARD_ENABLED')
 
 export const updateServerProxyUrlSetting = createAction('SETTINGS_UPDATE_SERVER_PROXY_URL')
