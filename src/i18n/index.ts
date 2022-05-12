4bed56274b55264e8012921dd441bd5483432e04
Adrian Aioanei <adrian.aioanei@ayonesoftware.com>
2022-05-12 19:13:13 +0300

    Added new languages.

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,14 +2,30 @@ import I18n from 'react-native-i18n'
 
 
 export const SupportedLocales: any = {
-  en: 'en',
+  da: 'da',
   de: 'de',
+  en: 'en',
+  es: 'es',
+  fr: 'fr',
+  it: 'it',
+  pt: 'pt',
+  ru: 'ru',
+  sl: 'sl',
+  zh: 'zh'
 }
 
 I18n.fallbacks = true
 I18n.translations = {
-  [SupportedLocales.en]: require('./translations/en.json'),
+  [SupportedLocales.da]: require('./translations/da.json'),
   [SupportedLocales.de]: require('./translations/de.json'),
+  [SupportedLocales.en]: require('./translations/en.json'),
+  [SupportedLocales.es]: require('./translations/es.json'),
+  [SupportedLocales.fr]: require('./translations/fr.json'),
+  [SupportedLocales.it]: require('./translations/it.json'),
+  [SupportedLocales.pt]: require('./translations/pt.json'),
+  [SupportedLocales.ru]: require('./translations/ru.json'),
+  [SupportedLocales.sl]: require('./translations/sl.json'),
+  [SupportedLocales.zh]: require('./translations/zh.json')
 }
 
 export default I18n
