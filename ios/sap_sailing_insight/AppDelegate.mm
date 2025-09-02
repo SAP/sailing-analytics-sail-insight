451d31a6cec4cf0c519298306aca692f0b5c80e4
michael.goertz <michael.goertz@insecia.com>
2025-09-02 15:54:04 +0200

    update IOS configs to start the app on IOS update fireBase packages and setup

--- ios/sap_sailing_insight/AppDelegate.mm
+++ ios/sap_sailing_insight/AppDelegate.mm
@@ -1,17 +1,44 @@
 #import "AppDelegate.h"
 #import <Firebase.h>
+#import <React/RCTBundleURLProvider.h>
+
+#if __has_include(<React/RCTAppSetupUtils.h>)
+#import <React/RCTAppSetupUtils.h>
+#endif
 
 @implementation AppDelegate
 
+- (NSURL *)bundleURL
+{
+#if DEBUG
+  #if REACT_NATIVE_MINOR_VERSION >= 75
+    // RN 0.75+
+    return [RCTBundleURLProvider.sharedSettings jsBundleURLForBundleRoot:@"index"];
+  #else
+    // RN <= 0.74
+    return [[RCTBundleURLProvider sharedSettings]
+              jsBundleURLForBundleRoot:@"index"
+                     fallbackResource:nil];
+  #endif
+#else
+  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+}
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  // Custom initialization
-  [FIRApp configure];
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
 
   self.moduleName = @"sap_sailing_insight";
   self.initialProps = @{};
 
-  // Delegate the rest to RCTAppDelegate
+  #if __has_include(<React/RCTAppSetupUtils.h>)
+    // Safe on RN ≥ 0.71; skipped on older RN if header isn't present
+    RCTAppSetupPrepareApp(application, YES);
+  #endif
+
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
